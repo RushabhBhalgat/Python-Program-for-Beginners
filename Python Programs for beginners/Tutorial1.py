9ad956fb91ae7f20e854221ef4dc17e12da7463e
RushabhBhalgat <65655146+RushabhBhalgat@users.noreply.github.com>
2020-08-07 16:37:46 +0530

    Add files via upload

---
+++ Python Programs for beginners/Tutorial1.py
@@ -0,0 +1,10 @@
+# Taking input from User
+num1 = input('Enter first number')
+num2 = input('Enter second number')
+
+# Add the number
+sum = float(num1) + float(num2)
+
+
+# Display result
+print('the addition of the numbers is ' , sum)
